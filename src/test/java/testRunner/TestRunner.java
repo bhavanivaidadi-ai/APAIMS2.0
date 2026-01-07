42023cb1a0f3d2d057a5591b1b1eef477c03d619
bhavanivaidadi-ai <bhavani.vaidadi@vassarlabs.com>
2026-01-07 17:58:25 +0530

    Added the required dependencies and generated the reports

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/featureFiles"},
-        glue = {"StepDefinition", "apphooks"},
+        glue = {"StepDefinition", "Apphooks"},
         plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "timeline:test-output/"
         },
