d31855e4a07d97f071c5a643c971c38fd5b78fc7
Ravali <ravali.gunishetty@vassarlabs.com>
2026-02-04 16:45:50 +0530

    new code addad

--- src/test/java/StepDefinition/ClusterDemonstrationStep.java
+++ src/test/java/StepDefinition/ClusterDemonstrationStep.java
@@ -4,6 +4,7 @@ import PageClasses.ClusterDemonstration;
 import PageClasses.M_E_PageClass;
 import driver.WebDriverInitializer;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class ClusterDemonstrationStep {
     ClusterDemonstration CD = new ClusterDemonstration(WebDriverInitializer.getDriver());
@@ -23,13 +24,21 @@ public class ClusterDemonstrationStep {
         CD.click_on_ClusterDemonstration_MIS();
     }
     @Then("Click on ClusterDemonstration District Target Report")
-    public void click_on_CD_District_Target_Report() {
+    public void click_on_CD_District_Target_Report() throws InterruptedException {
         CD.click_on_ClusterDemonstration_DistTR();
         Thread.sleep(1000);
     }
     @Then("Click on ClusterDemonstration Mandal Target Report")
-    public void click_on_CD_Mandal_Target_Report() {
+    public void click_on_CD_Mandal_Target_Report() throws InterruptedException {
         CD.click_on_ClusterDemonstration_MandalTR();
         Thread.sleep(1000);
     }
+    @Then("I should see the header as {string}")
+    public void i_should_see_the_header_as(String expectedText ) {
+        CD.verify_ClusterDemonstration_Header();
+
+    }
+
+
+
 }
